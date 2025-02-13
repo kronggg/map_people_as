5fa484fb7814c360d3693e18cd71d74f19afbc6d
Rom <106106275+kronggg@users.noreply.github.com>
2025-02-14 01:31:45 +0300

    Delete handlers directory

--- handlers/menu.py
+++
@@ -1,7 +0,0 @@
-def get_main_menu():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("👥 Мои связи", callback_data="my_connections")],
-        # ... остальные кнопки ...
-    ])
-    
-    
