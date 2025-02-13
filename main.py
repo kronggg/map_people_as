988de125c5c7aec3bf9d98b30a59d26581024a8f
Rom <106106275+kronggg@users.noreply.github.com>
2025-02-13 22:58:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,69 @@
 ##############################################
-# main.py - Инициализация и запуск бота
+# main.py - Главный модуль запуска бота
 ##############################################
 
-from config import Config
-from handlers import RegistrationHandlers
-from database import Database
-# ▼▼▼ Все импорты должны быть в начале файла ▼▼▼
-from handlers.connections import ConnectionHandlers
-from handlers.notifications import NotificationHandlers
 from telegram.ext import (
     Application,
     CommandHandler,
     ConversationHandler,
     MessageHandler,
-    filters,
-    CallbackQueryHandler
+    CallbackQueryHandler,
+    filters
 )
+from config import Config
+from handlers.registration import RegistrationHandlers
+from handlers.connections import ConnectionHandlers
+from handlers.notifications import NotificationHandlers
+from database.core import Database
+
+def setup_handlers(application):
+    """Регистрация всех обработчиков"""
+    
+    # Обработчик регистрации
+    reg_conv = ConversationHandler(
+        entry_points=[CommandHandler('start', RegistrationHandlers.start)],
+        states={
+            Config.GDPR_CONSENT: [
+                CallbackQueryHandler(RegistrationHandlers.handle_gdpr_choice)
+            ],
+            Config.ENTER_PHONE: [
+                MessageHandler(filters.TEXT, RegistrationHandlers.enter_phone)
+            ],
+            Config.VERIFY_OTP: [
+                MessageHandler(filters.TEXT, RegistrationHandlers.verify_otp)
+            ]
+        },
+        fallbacks=[CommandHandler('cancel', RegistrationHandlers.cancel)]
+    )
+    
+    # Обработчик связей
+    connection_conv = ConversationHandler(
+        entry_points=[CommandHandler('connect', ConnectionHandlers.init_connection)],
+        states={
+            'WAIT_SEARCH_QUERY': [
+                MessageHandler(filters.TEXT, ConnectionHandlers.handle_search_query)
+            ],
+            'HANDLE_CONNECTION_CHOICE': [
+                CallbackQueryHandler(ConnectionHandlers.handle_connection_choice)
+            ]
+        },
+        fallbacks=[CommandHandler('cancel', ConnectionHandlers.cancel)]
+    )
+
+    # Регистрация всех обработчиков
+    application.add_handlers([
+        reg_conv,
+        connection_conv,
+        CommandHandler('my_connections', ConnectionHandlers.show_connections),
+        CallbackQueryHandler(
+            NotificationHandlers.handle_connection_response,
+            pattern=r"^(accept|reject)_\d+$"
+        ),
+        CallbackQueryHandler(
+            ConnectionHandlers.handle_connections_pagination,
+            pattern=r"^conn_page_\d+$"
+        )
+    ])
 
 def main():
     # Инициализация приложения
@@ -24,50 +72,10 @@ def main():
     # Инициализация БД
     application.add_handler(CommandHandler('init_db', Database.init_db))
     
-    # Регистрация обработчиков регистрации
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', RegistrationHandlers.start)],
-        states={
-            # ... остальные состояния
-        },
-        fallbacks=[]
-    )
-    application.add_handler(conv_handler)
+    # Настройка обработчиков
+    setup_handlers(application)
     
-    # ▼▼▼ Регистрация обработчика связей ДО запуска бота ▼▼▼
-    connection_conv = ConversationHandler(
-        entry_points=[CommandHandler('connect', ConnectionHandlers.init_connection)],
-        states={
-            'WAIT_SEARCH_QUERY': [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, ConnectionHandlers.handle_search_query)
-            ],
-            'HANDLE_CONNECTION_CHOICE': [
-                CallbackQueryHandler(ConnectionHandlers.handle_connection_choice)
-            ]
-        },
-        fallbacks=[CommandHandler('cancel', ConnectionHandlers.cancel)]
-    )
-    application.add_handler(connection_conv)
-    
-    # Регистрация обработчика ответов
-    application.add_handler(CallbackQueryHandler(
-        NotificationHandlers.handle_connection_response,
-        pattern=r"^(accept|reject)_\d+$"
-    ))
-    
-    # Регистрация команды /my_connections
-    application.add_handler(CommandHandler(
-        "my_connections", 
-        ConnectionHandlers.show_connections
-    ))
-    
-    # Регистрация обработчика пагинации
-    application.add_handler(CallbackQueryHandler(
-        ConnectionHandlers.handle_connections_pagination,
-        pattern=r"^conn_page_\d+$"
-    ))
-    
-    # Запуск бота должен быть ПОСЛЕДНИМ ▼▼▼
+    # Запуск бота (всегда последний!)
     application.run_polling()
 
 if __name__ == "__main__":
