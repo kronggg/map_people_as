b2b497bac749a80b174abc7a209e1af31b6a9353
Rom <106106275+kronggg@users.noreply.github.com>
2025-02-14 16:17:07 +0300

    Delete utils directory
    Signed-off-by: Rom <106106275+kronggg@users.noreply.github.com>

--- utils/geocoder.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def geocode_city(city_name: str):
-    url = f"https://nominatim.openstreetmap.org/search?q={city_name}&format=json&limit=1"
-    response = requests.get(url).json()
-    if response:
-        location = response[0]
-        return float(location["lat"]), float(location["lon"])
-    return None
